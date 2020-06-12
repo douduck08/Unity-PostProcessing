70dd48daba96a71b201eedf4147e35885720d918
douduck08 <lu10082000@gmail.com>
2020-06-13 04:29:45 +0800

    add Noise

---
+++ Assets/Post Processing v2 Extension/Effects/Noise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnityEngine.Rendering.PostProcessing {
+    [Serializable]
+    [PostProcess (typeof (NoiseRenderer), PostProcessEvent.AfterStack, "Toys/Noise")]
+    public sealed class Noise : PostProcessEffectSettings {
+        public FloatParameter scale = new FloatParameter () { value = 1f };
+    }
+
+    public sealed class NoiseRenderer : PostProcessEffectRenderer<Noise> {
+
+        Shader shader;
+
+        public override void Init () {
+            shader = Shader.Find ("Hidden/Noise");
+        }
+
+        public override void Render (PostProcessRenderContext context) {
+            var sheet = context.propertySheets.Get (shader);
+            sheet.properties.SetFloat ("_Scale", settings.scale);
+
+            var cmd = context.command;
+            cmd.BeginSample ("Noise");
+            cmd.BlitFullscreenTriangle (context.source, context.destination, sheet, 0);
+            cmd.EndSample ("Noise");
+        }
+    }
+}
---
+++ Assets/Post Processing v2 Extension/Resources/Noise.shader
@@ -0,0 +1,28 @@
+﻿Shader "Hidden/Noise" {
+    SubShader {
+        Cull Off ZWrite Off ZTest Always
+        Pass {
+            HLSLPROGRAM
+            #pragma vertex VertDefault
+            #pragma fragment frag
+
+            #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+            TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+            float4 _MainTex_TexelSize;
+
+            float _Scale;
+
+            float Hash (float2 seed) {
+                return frac(sin(dot(seed, float2(12.9898, 78.233))) * 43758.5453);
+            }
+
+            half4 frag (VaryingsDefault i) : SV_Target {
+                float2 uv = i.texcoord * _Scale;
+                half4 color = Hash(uv);
+                return color;
+            }
+            ENDHLSL
+        }
+    }
+}
